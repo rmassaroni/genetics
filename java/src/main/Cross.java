5a9c6e1ba37f3ba16fa47052dc839efa0b278a2e
rjm <robjmass@gmail.com>
2024-03-23 16:03:58 -0400

    Cross.java

---
+++ java/src/main/Cross.java
@@ -0,0 +1,9 @@
+package main;
+
+import Genotype;
+
+public class Cross {
+    public Cross() {
+
+    }
+}
--- java/src/main/Genotype.java
+++ java/src/main/Genotype.java
@@ -1,7 +1,11 @@
+package main;
+
 public class Genotype {
     public String name;
+    public char[] alleles;
 
     public Genotype(String name) {
         this.name = name;
+        alleles = name.toCharArray();
     }
 }
